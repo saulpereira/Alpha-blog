b34df288d9053ca50fc4270d5d936909b7cfb683
spereira <saul_pereira@hotmail.com>
2020-02-13 16:10:53 -0600

    Upgrading to bootstrap 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
+//= require popper
 //= require bootstrap-sprockets
-//= require activestorage
 //= require turbolinks
 //= require_tree .
